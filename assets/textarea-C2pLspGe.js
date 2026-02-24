bf590c0b9ff8843253e647c338ecf577791e0cb6
pitsecommerce-beep <pitsecommerce-beep@users.noreply.github.com>
2026-02-24 05:32:13 +0000

    deploy: 306dd06a6e1a1cd01cd1bd988774c3b1b364d15f

---
+++ assets/textarea-C2pLspGe.js
@@ -0,0 +1,6 @@
+import{k as s,r as t,j as o,l as i}from"./index-CEkNgELi.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z",key:"1a8usu"}],["path",{d:"m15 5 4 4",key:"1mk7zo"}]],d=s("Pencil",n),l=t.forwardRef(({className:e,...a},r)=>o.jsx("textarea",{className:i("flex min-h-[80px] w-full rounded-lg border border-white/15 bg-white/5 px-3 py-2 text-sm text-gray-100 placeholder:text-gray-500","ring-offset-transparent transition-colors resize-none","focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand-500 focus-visible:border-transparent","disabled:cursor-not-allowed disabled:opacity-50",e),ref:r,...a}));l.displayName="Textarea";export{d as P,l as T};
